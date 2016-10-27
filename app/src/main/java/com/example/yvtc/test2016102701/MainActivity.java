72ec445e98631c7fe878aadfab8f7daaffefcaf8
pohung <kof96302@yahoo.com.tw>
2016-10-27 19:37:51 +0800

    SeekBar

--- app/src/main/java/com/example/yvtc/test2016102701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/test2016102701/MainActivity.java
@@ -4,15 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar pb;
+    ProgressBar pb,pb2;
+    SeekBar sb;
+    TextView tv1,tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb2 = (ProgressBar) findViewById(R.id.progressBar2);
+        sb = (SeekBar)findViewById(R.id.seekBar);
+        tv1 = (TextView)findViewById(R.id.textView2);
+        tv2 = (TextView)findViewById(R.id.textView3);
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv1.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
     public void clickShow(View v)
     {
@@ -42,4 +68,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
     }
+    public void click2(View v)
+    {
+        pb2.setProgress(pb2.getProgress()+10);
+    }
+
 }
